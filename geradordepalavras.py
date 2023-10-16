fddb8fbfbb9a62dd3f77f7fad30a64575354dc42
Matheus Lopes <130188560+matheusfsanna@users.noreply.github.com>
2023-10-16 13:43:10 -0300

    gaerador_de_palavras_gpt.py
    Adicionada a função de gerar palavras com o ChatGPT

--- geradordepalavras.py
+++ geradordepalavras.py
@@ -1,104 +1,56 @@
+import openai
+import dotenv
 import pandas as pd
+import os
 
-# Lista de 30 palavras
-palavras = [
-    "algoritmo",
-    "programação",
-    "desenvolvedor",
-    "código",
-    "software",
-    "hardware",
-    "internet",
-    "segurança",
-    "criptografia",
-    "nuvem",
-    "servidor",
-    "protocolo",
-    "firewall",
-    "cache",
-    "interface",
-    "usuário",
-    "acessibilidade",
-    "inovação",
-    "automação",
-    "robótica",
-    "inteligência",
-    "artificial",
-    "aprendizado",
-    "máquina",
-    "realidade",
-    "virtual",
-    "deep learning",
-    "blockchain",
-    "dispositivo",
-    "móvel",
-    "aplicativo",
-    "atualização",
-    "sistema",
-    "operacional",
-    "integração",
-    "api",
-    "backend",
-    "frontend",
-    "website",
-    "e-commerce",
-    "programador",
-    "engenharia",
-    "de software",
-    "linguagem",
-    "compilação",
-    "depuração",
-    "bug",
-    "computação",
-    "quântica",
-    "nanotecnologia",
-    "realidade",
-    "aumentada",
-    "big data",
-    "análise",
-    "dados",
-    "cibersegurança",
-    "vulnerabilidade",
-    "ataque",
-    "phishing",
-    "malware",
-    "criptomoeda",
-    "bitcoin",
-    "ethereum",
-    "5g",
-    "iot",
-    "automação",
-    "veicular",
-    "impressão",
-    "3d",
-    "drone",
-    "realidade",
-    "mista",
-    "biometria",
-    "reconhecimento",
-    "facial",
-    "assistente",
-    "virtual",
-    "machine",
-    "vision",
-    "de recomendação",
-    "computação",
-    "em nuvem",
-    "geolocalização",
-    "realidade",
-    "aumentada",
-    "desenvolvimento",
-    "ágil",
-    "sprint",
-    "escopo",
-    "teste",
-    "qualidade",
-    "usabilidade",
-    "experiência",
-    "do usuário"
-]
+dotenv.load_dotenv()
 
-df = pd.DataFrame({'Palavra': palavras})
+openai.api_key = os.getenv("openai.api_key")
+openai.organization = os.getenv("org-c0wUdF4mxq11k27ojyd5xysK")
 
-# Salvar o DataFrame em um arquivo CSV sem a coluna de índice
+#openai.api_key = "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"
+
+# Função para obter o tema escolhido pelo usuário
+def obter_tema_do_usuario():
+    return input("Digite o tema desejado para gerar palavras: ")
+
+# Obtém o tema escolhido pelo usuário
+tema_escolhido = obter_tema_do_usuario()
+
+# Cria a solicitação para o ChatGPT
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "system",
+            "content": f"Gere palavras, com índice, para um jogo da forca com o tema {tema_escolhido}"
+        },
+        {
+            "role": "user",
+            "content": f"Gere 10 palavras para um jogo da forca com o tema {tema_escolhido}"
+        }
+    ],
+    temperature=1,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
+
+# Processa a resposta para criar um dicionário com índice numérico
+def extrair_palavras_com_indice(resposta):
+    linhas = resposta['choices'][0]['message']['content'].split('\n')
+    palavras_com_indice = [linha.split('. ')[1] for linha in linhas if linha.startswith(('\d', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))]
+    dicionario_palavras = {indice: palavra.upper() for indice, palavra in enumerate(palavras_com_indice, 1)}
+    return dicionario_palavras
+
+# Usa a função para obter o dicionário de palavras com índice
+palavras_com_indice = extrair_palavras_com_indice(response)
+
+# Exibe o dicionário de palavras com índice
+df = pd.DataFrame(list(palavras_com_indice.items()), columns=['Índice', 'Palavra'])
+
+# Salva o DataFrame em um arquivo CSV
 df.to_csv('palavras.csv', index=False)
+
+print(f"Arquivo CSV 'palavras.csv' salvo com sucesso para o tema '{tema_escolhido}'.")
