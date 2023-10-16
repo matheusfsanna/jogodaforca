5005a102562f270045ccfa24c395b1fd3ac0341f
Matheus Lopes <130188560+matheusfsanna@users.noreply.github.com>
2023-10-16 13:30:17 -0300

    jogo_da_forca.py
    Corrigido alguns bugs e adicionada a opção de jogar mais de uma vez

--- jogoforca.py
+++ jogoforca.py
@@ -8,108 +8,97 @@ Interface gráfica criada pelo CHATGPT
 import random
 import pandas as pd
 from unidecode import unidecode
-import tkinter as tk
-from tkinter import messagebox
 
-class JogoDaForca:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Jogo da Forca")
+def importar_csv(caminho_arquivo):
+    try:
+        # Lê o arquivo CSV e cria um DataFrame sem índice
+        df = pd.read_csv('palavras.csv', delimiter = ',', index_col=None)
+        return df
 
-        self.palavras = self.importar_csv('palavras.csv')
-        self.palavra_a_adivinhar = ""
-        self.letras_tentadas = []
-        self.tentativas_maximas = 6
-        self.tentativas = 0
+    except FileNotFoundError:
+        print(f"Arquivo não encontrado: {caminho_arquivo}")
+    except Exception as e:
+        print(f"Erro ao importar o arquivo CSV: {e}")
+    
+def obter_palavra(): 
+    global palavras
+    palavras = importar_csv('palavras.csv')
+    #print(palavras)
+    a = random.randint(0, len(palavras)-1)
+    palavra = unidecode(palavras['Palavra'][a].lower())
+    #print(f'A palavra escolhida foi: {palavra}')
+    
+    return palavra
 
-        self.iniciar_jogo()
-
-    def importar_csv(self, caminho_arquivo):
-        try:
-            # Lê o arquivo CSV e cria um DataFrame sem índice
-            df = pd.read_csv(caminho_arquivo, delimiter=',', index_col=None)
-            return df
-        except FileNotFoundError:
-            messagebox.showerror("Erro", f"Arquivo não encontrado: {caminho_arquivo}")
-        except Exception as e:
-            messagebox.showerror("Erro", f"Erro ao importar o arquivo CSV: {e}")
-
-    def obter_palavra(self):
-        a = random.randint(0, len(self.palavras) - 1)
-        self.palavra_a_adivinhar = unidecode(self.palavras['Palavra'][a].lower())
-
-    def exibir_palavra_oculta(self):
-        palavra_oculta = ""
-        for letra in self.palavra_a_adivinhar:
-            if letra in self.letras_tentadas or letra.isspace():
-                palavra_oculta += letra
-            else:
-                palavra_oculta += "_"
-        return palavra_oculta
-
-    def validar(self, letra):
-        if len(letra) == 1 and letra.isalpha():
-            letra_validada = unidecode(letra.lower())
-            return letra_validada
+def exibir_palavra_oculta(palavra, letras_tentadas):
+    palavra_oculta = ""
+    for letra in palavra:
+        if letra in letras_tentadas or letra.isspace():
+            palavra_oculta += letra
         else:
-            messagebox.showwarning("Aviso", "Por favor, digite exatamente uma letra.")
+            palavra_oculta += "_"
+    return palavra_oculta
 
-    def adivinhar_letra(self):
-        letra = self.entry_letra.get()
-        letra_validada = self.validar(letra)
+def validar():
+    
+    # Receber entrada do usuário
+ while True:   
+    letra = input("Digite uma letra: ")
+    if len(letra) == 1 and letra.isalpha():
+        letra_validada = unidecode(letra.lower())
+        return letra_validada
+        break
+    
+    else:
+        print("Por favor, digite exatamente uma letra.")
+    
+def jogo_da_forca(palavra):
+    palavra_a_adivinhar = palavra.lower()
+    letras_tentadas = []
+    tentativas_maximas = 6
+    tentativas = 0
 
-        if letra_validada:
-            # Verifica se a letra já foi tentada
-            if letra_validada in self.letras_tentadas:
-                messagebox.showwarning("Aviso", "Você já tentou essa letra. Tente outra.")
-            else:
-                self.letras_tentadas.append(letra_validada)
+    print("Bem-vindo ao Jogo da Forca!")
+    print(exibir_palavra_oculta(palavra_a_adivinhar, letras_tentadas))
 
-                # Verifica se a letra está na palavra
-                if letra_validada in self.palavra_a_adivinhar:
-                    messagebox.showinfo("Informação", f"Letra '{letra_validada}' correta!")
-                else:
-                    self.tentativas += 1
-                    messagebox.showinfo("Informação", f"Letra '{letra_validada}' incorreta. Tentativas restantes: "
-                                                    f"{self.tentativas_maximas - self.tentativas}")
+    while tentativas < tentativas_maximas:
+       #letra_sem_validacao = input("\nDigite uma letra: ").lower()
+        letra = validar()
 
-                # Exibe a palavra oculta atualizada
-                palavra_oculta = self.exibir_palavra_oculta()
-                self.label_palavra.config(text=palavra_oculta)
+        # Verifica se a letra já foi tentada
+        if letra in letras_tentadas:
+            print("Você já tentou essa letra. Tente outra.")
+            continue
 
-                # Verifica se o jogador adivinhou a palavra
-                if palavra_oculta == self.palavra_a_adivinhar:
-                    messagebox.showinfo("Parabéns!", "Você adivinhou a palavra.")
-                    self.palavras['Palavra'] = self.palavras['Palavra'].str.replace(self.palavra_a_adivinhar, '')
-                    self.palavras = self.palavras.dropna()
-                    self.palavras.to_csv('palavras.csv', index=False)
-                    self.iniciar_jogo()
+        letras_tentadas.append(letra)
 
-                # Verifica se o jogador atingiu o número máximo de tentativas
-                elif self.tentativas == self.tentativas_maximas:
-                    messagebox.showinfo("Game Over", f"A palavra era: {self.palavra_a_adivinhar}")
-                    self.iniciar_jogo()
+        # Verifica se a letra está na palavra
+        if letra in palavra_a_adivinhar:
+            print(f"Letra '{letra}' correta!")
+        else:
+            tentativas += 1
+            print(f"Letra '{letra}' incorreta. Tentativas restantes: {tentativas_maximas - tentativas}")
 
-    def iniciar_jogo(self):
-        self.tentativas = 0
-        self.letras_tentadas = []
-        self.obter_palavra()
+        # Exibe a palavra oculta atualizada
+        palavra_oculta = exibir_palavra_oculta(palavra_a_adivinhar, letras_tentadas)
+        print(palavra_oculta)
 
-        # Configuração da interface gráfica
-        self.label_palavra = tk.Label(self.root, text=self.exibir_palavra_oculta(), font=('Arial', 16))
-        self.label_palavra.pack(pady=10)
+        # Verifica se o jogador adivinhou a palavra
+        if palavra_oculta == palavra_a_adivinhar:
+            print("Parabéns! Você adivinhou a palavra.")
+            palavras['Palavra'] = palavras['Palavra'].str.replace(palavra,'')
+            palavras_sem_nan = palavras.dropna()
+            palavras_sem_nan.to_csv('palavras.csv', index=False)
+            break
 
-        self.label_letra = tk.Label(self.root, text="Digite uma letra:", font=('Arial', 12))
-        self.label_letra.pack(pady=5)
+    else:
+        print(f"\nGame Over! A palavra era: {palavra_a_adivinhar}")
 
-        self.entry_letra = tk.Entry(self.root, font=('Arial', 12))
-        self.entry_letra.pack(pady=5)
-
-        self.button_adivinhar = tk.Button(self.root, text="Adivinhar Letra", command=self.adivinhar_letra, font=('Arial', 12))
-        self.button_adivinhar.pack(pady=10)
-
-        self.root.mainloop()
-
-# Criar a instância do jogo
-root = tk.Tk()
-jogo = JogoDaForca(root)
+# Chama o jogo
+while True:
+    a = input("Digite qualquer tecla para jogar ou 0 para sair: ")
+    if a == '0':
+        break
+    else:
+        palavra = obter_palavra()
+        jogo_da_forca(palavra)
